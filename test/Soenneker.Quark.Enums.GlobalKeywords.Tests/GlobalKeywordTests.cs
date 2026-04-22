d91aeaa55c88b7d658a0f912449028113028668e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:28 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.GlobalKeywords.Tests/GlobalKeywordTests.cs
+++ test/Soenneker.Quark.Enums.GlobalKeywords.Tests/GlobalKeywordTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.GlobalKeywords.Tests;
 
 public sealed class GlobalKeywordTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
